e0168388a5adec9864601d12355a5d93d3759af3
René Winkelmeyer <muenzpraeger@users.noreply.github.com>
2021-02-03 11:23:08 +0100

    feat: make string linting opt-in (#28)
    * feat: make string linting configurable
    
    * feat: update unit tests

--- tests/lib/rules/use-inclusive-words-autofix.test.js
+++ tests/lib/rules/use-inclusive-words-autofix.test.js
@@ -2,7 +2,7 @@
 
 const rule = require('../../../lib/rules/use-inclusive-words');
 
-const customConfigDefault = {
+const customConfigAutofix = {
     words: [
         {
             word: 'guys',
@@ -18,7 +18,9 @@ const customConfigDefault = {
         { term: 'definitiely-partial-guys', allowPartialMatches: true },
         { term: 'not-partial-guys', allowPartialMatches: false },
         { term: 'notslugpartialguys', allowPartialMatches: true }
-    ]
+    ],
+    autofix: true,
+    lintStrings: true
 };
 
 const RuleTester = require('eslint').RuleTester;
@@ -38,84 +40,77 @@ ruleTester.run('use-inclusive-words', rule, {
         },
         {
             code: 'var message = "This is a group of people."',
-            options: [customConfigDefault]
+            options: [customConfigAutofix]
         },
         {
             code: 'var ccType = "MasterFoo"',
-            options: [customConfigDefault]
+            options: [customConfigAutofix]
         },
         {
             code: 'var fooMaster = 1',
-            options: [customConfigDefault]
+            options: [customConfigAutofix]
         },
         {
             code: 'var BazMasterBar = function () {}',
-            options: [customConfigDefault]
+            options: [customConfigAutofix]
         },
         {
             code:
                 '/* This is an example of non-partial matching (not-partial-guys) */',
-            options: [customConfigDefault]
+            options: [customConfigAutofix]
         },
         {
             code:
                 '/* This is an example of partial matching (extra-definitiely-partial-guys) */',
-            options: [customConfigDefault]
+            options: [customConfigAutofix]
         },
         {
             code:
                 '// A comment with a a url https://myvcs.com/someAccount/blob/master/README.md',
-            options: [customConfigDefault]
+            options: [customConfigAutofix]
         },
         {
             code: 'var something_notslugpartialguys = 323',
-            options: [customConfigDefault]
-        },
-        {
-            code: 'var message = "This is a group of guys."',
-            options: [customConfigDefault]
-        },
-        {
-            code: 'var message = "made-it-partial-not-partial-guys"',
-            options: [customConfigDefault]
-        },
-        {
-            code: 'var classname = "master-bar"'
+            options: [customConfigAutofix]
         }
     ],
     invalid: [
         {
             code: 'var isBlacklisted = true',
+            options: [customConfigAutofix],
             errors: [
                 {
                     message:
                         "To convey the same idea, consider 'blocklist' instead of 'blacklist'."
                 }
             ],
-            output: 'var isBlacklisted = true'
+            output: 'var isBlocklisted = true'
         },
         {
             code: 'function rulesUpdate(whitelist) {}',
+            options: [customConfigAutofix],
             errors: [
                 {
                     message:
                         "To convey the same idea, consider 'allowlist' instead of 'whitelist'."
                 }
             ],
-            output: 'function rulesUpdate(whitelist) {}'
+            output: 'function rulesUpdate(allowlist) {}'
         },
         {
             code: '// This updates the master branch of the repository.',
+            options: [customConfigAutofix],
             errors: [
                 {
                     message: "Instead of 'master', you can use 'primary'."
                 }
             ],
-            output: '// This updates the master branch of the repository.'
+            output: '// This updates the primary branch of the repository.'
         },
         {
             code:
                 'var sendUpdate = isMasterConnected ? true : isSlaveConnected',
+            options: [customConfigAutofix],
             errors: [
                 {
                     message: "Instead of 'master', you can use 'primary'."
@@ -126,29 +121,69 @@ ruleTester.run('use-inclusive-words', rule, {
                 }
             ],
             output:
-                'var sendUpdate = isMasterConnected ? true : isSlaveConnected'
+                'var sendUpdate = isPrimaryConnected ? true : isSecondaryConnected'
+        },
+        {
+            code: 'var message = "This is a group of guys."',
+            options: [customConfigAutofix],
+            errors: [
+                {
+                    message: "Instead of 'guys', you can use 'people'."
+                }
+            ],
+            output: 'var message = "This is a group of people."'
         },
         {
             code: 'var fooMasters = 1',
-            options: [customConfigDefault],
+            options: [customConfigAutofix],
             errors: [
                 {
                     message: "Instead of 'master', you can use 'primary'."
                 }
             ],
-            output: 'var fooMasters = 1'
+            output: 'var fooPrimarys = 1'
+        },
+        {
+            code: 'var message = "made-it-partial-not-partial-guys"',
+            options: [customConfigAutofix],
+            errors: [
+                {
+                    message: "Instead of 'guys', you can use 'people'."
+                }
+            ],
+            output: 'var message = "made-it-partial-not-partial-people"'
+        },
+        {
+            code: 'var classname = "master-bar"',
+            options: [customConfigAutofix],
+            errors: [
+                {
+                    message: "Instead of 'master', you can use 'primary'.",
+                    suggestions: [
+                        {
+                            desc: "Replace word 'master' with 'primary.'"
+                        },
+                        {
+                            desc: "Replace word 'master' with 'main.'"
+                        }
+                    ]
+                }
+            ],
+            output: 'var classname = "primary-bar"'
         },
         {
             code: '<MasterClass />',
+            options: [customConfigAutofix],
             errors: [
                 {
                     message: "Instead of 'master', you can use 'primary'."
                 }
             ],
-            output: '<MasterClass />'
+            output: '<PrimaryClass />'
         },
         {
             code: '<Foo blacklist={blacklist} />',
+            options: [customConfigAutofix],
             errors: [
                 {
                     message:
@@ -159,7 +194,7 @@ ruleTester.run('use-inclusive-words', rule, {
                         "To convey the same idea, consider 'blocklist' instead of 'blacklist'."
                 }
             ],
-            output: '<Foo blacklist={blacklist} />'
+            output: '<Foo blocklist={blocklist} />'
         }
     ]
 });
